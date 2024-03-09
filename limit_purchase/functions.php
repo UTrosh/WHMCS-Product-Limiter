f00989ac4408904c9acbe4cddc8522ee9fe7ef1c
UTrosh <73399986+UTrosh@users.noreply.github.com>
2024-03-09 17:33:49 +0100

    Update functions.php

--- limit_purchase/functions.php
+++ limit_purchase/functions.php
@@ -1,70 +1,63 @@
 <?php
+use WHMCS\Database\Capsule as Capsule;
 
-if (!defined("WHMCS"))
-	die("This file cannot be accessed directly");
-
-class limit_purchase
-{
-	var $config;
-
-	function __construct()
-	{
-		$this->loadConfig();
-	}
-
-	function loadConfig()
-	{
-		$this->config = array();
-
-		$sql = "SELECT *
-			FROM mod_limit_purchase_config";
-		$result = mysql_query($sql);
-
-		while($config_details = mysql_fetch_assoc($result))
-		{
-			$this->config[$config_details['name']] = $config_details['value'];
-		}
-		mysqli_free_result($result);
-	}
-
-	function setConfig($name, $value)
-	{
-		if(isset($this->config[$name]))
-		{
-			$sql = "UPDATE mod_limit_purchase_config
-				SET value = '" . mysqli_real_escape_string($value) . "'
-				WHERE name = '" . mysqli_real_escape_string($name) . "'";
-			$result = mysql_query($sql);
-		}
-		else
-		{
-			$sql = "INSERT INTO mod_limit_purchase_config (`name`,`value`) VALUES
-				('" . mysqli_real_escape_string($name) . "','" . mysqli_real_escape_string($value) . "')";
-			$result = mysql_query($sql);
-		}
-
-		$this->config[$name] = $value;
-	}
-
-	function getLimitedProducts()
-	{
-		$output = array();
-
-		$sql = "SELECT l.*
-			FROM mod_limit_purchase as l
-			INNER JOIN tblproducts as p
-			ON p.id = l.product_id
-			WHERE l.active = 1";
-		$result = mysql_query($sql);
-
-		while($limits = mysql_fetch_assoc($result))
-		{
-			$output[$limits['product_id']] = array('limit' => $limits['limit'], 'error' => $limits['error']);
-		}
-		mysqli_free_result($result);
-
-		return $output;
-	}
+if (!defined("WHMCS")) {
+    die("Ce fichier ne peut pas être accédé directement");
 }
 
-?>
+class LimitPurchase
+{
+    public $config;
+
+    public function __construct()
+    {
+        $this->loadConfig();
+    }
+
+    protected function loadConfig()
+    {
+        $this->config = array();
+
+        $pdo = Capsule::connection()->getPdo();
+
+        $sql = "SELECT * FROM mod_limit_purchase_config";
+        $statement = $pdo->query($sql);
+
+        while ($config_details = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $this->config[$config_details['name']] = $config_details['value'];
+        }
+    }
+
+    public function setConfig($name, $value)
+    {
+        $pdo = Capsule::connection()->getPdo();
+
+        if (isset($this->config[$name])) {
+            $sql = "UPDATE mod_limit_purchase_config SET value = :value WHERE name = :name";
+            $statement = $pdo->prepare($sql);
+            $statement->execute(['value' => $value, 'name' => $name]);
+        } else {
+            $sql = "INSERT INTO mod_limit_purchase_config (`name`, `value`) VALUES (:name, :value)";
+            $statement = $pdo->prepare($sql);
+            $statement->execute(['name' => $name, 'value' => $value]);
+        }
+
+        $this->config[$name] = $value;
+    }
+
+    public function getLimitedProducts()
+    {
+        $output = array();
+
+        $pdo = Capsule::connection()->getPdo();
+
+        $sql = "SELECT l.* FROM mod_limit_purchase as l INNER JOIN tblproducts as p ON p.id = l.product_id WHERE l.active = 1";
+        $statement = $pdo->query($sql);
+
+        while ($limits = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $output[$limits['product_id']] = array('limit' => $limits['limit'], 'error' => $limits['error']);
+        }
+
+        return $output;
+    }
+}
